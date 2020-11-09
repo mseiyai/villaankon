4a4d2b87129000fb62ac54504cc2a07b76a687ac
mseiyai <mseiyai@gmail.com>
2020-11-09 13:33:09 +0300

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="h-screen flex flex-col justify-center items-center">
+    <slot />
+  </main>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style></style>
